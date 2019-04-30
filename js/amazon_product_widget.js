818aa9d9953e2f638718c33f2eaeb2e8d1f8b571
Arthur Lorenz <arthur@drunomics.com>
2019-04-30 11:32:27 +0200

    Fixed check for empty product field.

--- js/amazon_product_widget.js
+++ js/amazon_product_widget.js
@@ -10,7 +10,7 @@
    * @param field
    */
   var loadAmazonWidget = function (field) {
-    if (field.innerHTML === "") {
+    if (field.innerHTML.trim() === "") {
       var xhr = new XMLHttpRequest();
       xhr.open('GET', '/api/amazon/product?entity_id=' + field.dataset.entityId + '&entity_type=' + field.dataset.entityType + '&field=' + field.dataset.field, true);
       xhr.onload = function() {
