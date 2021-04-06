fb9bb9f93365296a27b4c5a30d4df1423489384b
Mathias Müller <mathias.mueller@drunomics.com>
2021-04-06 16:56:32 +0200

    Add config setting to disable amazon api.

--- src/Exception/AmazonRequestLimitReachedException.php
+++ src/Exception/AmazonRequestLimitReachedException.php
@@ -2,7 +2,18 @@
 
 namespace Drupal\amazon_product_widget\Exception;
 
+use Throwable;
+
 /**
  * Amazon API request limit reached.
  */
-class AmazonRequestLimitReachedException extends AmazonServiceException {}
+class AmazonRequestLimitReachedException extends AmazonServiceException {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function __construct($message = "Maximum number of requests per day to Amazon API reached.", $code = 0, Throwable $previous = null) {
+    parent::__construct($message, $code, $previous);
+  }
+
+}
